aea04995f3f96fc4e87edebe587cc64ba8431d23
wangzhaoshuai <zhaoshuai.wang@bjsobf.com>
2016-11-01 10:06:59 +0800

    Initial Commit

---
+++ G1KSlideAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G1KSlideAnimation
+//
+//  Created by Sobf Leo on 16/11/1.
+//  Copyright © 2016年 SOBF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ G1KSlideAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  G1KSlideAnimation
+//
+//  Created by Sobf Leo on 16/11/1.
+//  Copyright © 2016年 SOBF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
